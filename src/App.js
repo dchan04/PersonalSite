8672437428c83ad906a6d17815d3982bb7996a8f
dchan04 <darrenchan1123@gmail.com>
2023-03-11 12:13:38 -0500

    Made a lot of progress on the Home page design. Still needs work on functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderComponent from './Header';
-import Home from './Home'
+import Home from './Home';
+import AboutMe from './About';
 function App() {
     return (
 
         <div className="App">
             <HeaderComponent />
             <Home />
+            <AboutMe />
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,39 @@
 import "./Home.css";
+import githubIcon from "./assets/github-mark.png"
+import linkedInIcon from "./assets/linkedin.png"
+import Image from 'react-bootstrap/Image'
 
 function HomePage() {
     return (
         <div className="HomeContainer">
             <div className="intro">Hello, My Name is Darren Chan</div>
-            <div className="description">I am a Junior Developer</div>
-            <div className="overviewButton">Go To Overview</div>
+            <div className="description">I'm a software developer with fundamental knowledge of Web Development using React and ASP.Net.</div>
+            <button size="lg" variant="link" className="rounded-Button">
+                <Image className="linkIcons" src={githubIcon} ></Image>
+            </button>
+            <button size="lg" variant="link" className="rounded-Button2">
+                <Image className="linkIcons" src={linkedInIcon} ></Image>
+            </button>
             <div className="homePic">
                 <div className="redButton" />
                 <div className="yellowButton" />
                 <div className="greenButton" />
                 <div className="url">DarrenChan.ca</div>
-                <div className="education">
-                    <p className="purpleText">Education</p>
-                    <p className="whiteText">{`{`}</p>
-                    <p className="category">Profession:</p>
-                    <p className="category">Languages:</p>
+                <div className="skillsCategory">
+                    <div className="purpleText">Skills</div> 
+                    <div className="firstBracket">{`{`}</div>
+                    <div className="category">Languages =<span className="categoryText">HTML + CSS + JavaScript + C# + +jQuery + SQL;</span> </div> 
+                    <div className="category">Frameworks =<span className="categoryText">React.JS + .Net Framework + Bootstrap 5 + Node.js;</span> </div> 
+                    <div className="category">Databases =<span className="categoryText">MySql + SQLite + MongoDB;</span> </div> 
                     <p className="endBracket">{`}`}</p>
                 </div>
-                <div className="aboutMe">
-                    <p className="purpleText">About Me</p>
-                    <p className="whiteText">{`{`}</p>
-                    <p className="category">Education:</p>
-                    <p className="endBracket">{`}`}</p>
+                <div className="educationCategory">
+                    <div className="purpleText">Education</div>
+                    <div className="firstBracket">{`{`}</div>
+                    <div className="category">Degree =<span className="categoryText">Bachelor of Computing;</span></div>
+                    <div className="category">Major =<span className="categoryText">Computer Science;</span></div>
+                    <div className="category">Minor =<span className="categoryText">Business Economics;</span></div>
+                    <div className="endBracket">{`}`}</div>
                 </div>
             </div>
         </div>
