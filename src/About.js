203e489671724efd344dc6e9079705be2d3c6bd8
dchan04 <darrenchan1123@gmail.com>
2023-03-29 13:35:27 -0400

    Remade logo and changed it to svg file to make it look cleaner. Cleaned up css files. Added animations to our header links.

--- src/About.js
+++ src/About.js
@@ -1,50 +1,114 @@
 import "./About.css";
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
+import { Button } from "react-bootstrap";
 
 function About() {
-    const ref = useRef(null)
-    const isInView = useInView(ref, { once: true })
-    return (
-        <div className="AboutContainer">
-            <motion.div
-                className="Title"
-                ref={ref}
-                style={{
-                    transform: isInView ? "translateZ(0) translateY(0)" : "translateZ(-700px) translateY(500px)",
-                    opacity: isInView ? 1 : 0,
-                    transition: "all 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.5s"
-                }}>
-                <motion.span>
-                    About Me
-                </motion.span>
-            </motion.div>
-            <div className="Description">
-            filler
-            </div>
-            <div className="skillContainer">
-                <div className="redButton" />
-                <div className="yellowButton" />
-                <div className="greenButton" />
-                <div className="url">DarrenChan.ca</div>
-                <div className="skillsCategory">
-                    <div className="purpleText">Skills</div>
-                    <div className="firstBracket">{`{`}</div>
-                    <div className="category">Languages =<span className="categoryText">HTML + CSS + JavaScript + C# + SQL;</span> </div>
-                    <div className="category">Frameworks =<span className="categoryText">React.JS + .Net Framework + Bootstrap 5 + Node.js;</span> </div>
-                    <div className="category">Databases =<span className="categoryText">MySql + SQLite + MongoDB;</span> </div>
-                    <p className="endBracket">{`}`}</p>
-                </div>
-                <div className="educationCategory">
-                    <div className="purpleText">Education</div>
-                    <div className="firstBracket">{`{`}</div>
-                    <div className="category">Degree =<span className="categoryText">Bachelor of Computing;</span></div>
-                    <div className="category">Major =<span className="categoryText">Computer Science;</span></div>
-                    <div className="category">Minor =<span className="categoryText">Business Economics;</span></div>
-                    <div className="endBracket">{`}`}</div>
-                </div>
-            </div>
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  return (
+    <div className="AboutContainer">
+      <motion.div
+        className="Title"
+        ref={ref}
+        style={{
+          transform: isInView
+            ? "translateZ(0) translateY(0)"
+            : "translateZ(-700px) translateY(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000)",
+        }}
+      >
+        <motion.p>About Me</motion.p>
+      </motion.div>
+      <motion.div
+        className="Description"
+        style={{
+          transform: isInView
+            ? "translateZ(0) translateY(0)"
+            : "translateZ(-700px) translateY(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.2s",
+        }}
+      >
+        <p>
+          Hello, my name is Darren Chan. I completed my Bachelor of Computing at
+          University of Guelph, Majoring in Computer Science and Minoring in
+          Business Economics.
+        </p>
+
+        <p>
+          After University, I began leaning more towards Web Development. This
+          interest led me to learning <span id="reactLogoColor">React.js</span>{" "}
+          for front-end development and <span id="aspLogoColor">ASP.NET</span>{" "}
+          for back-end development.
+        </p>
+
+        <p>
+          I'm open to all Job opportunities where I can contribute, learn and
+          grow. If you think you have an opportunity that I would be best suited
+          for, feel free to contact me!
+        </p>
+        <div>
+          <a href="" className="contact-button">
+            Contact
+          </a>
         </div>
+      </motion.div>
+      <motion.div
+        className="skillContainer"
+        style={{
+          transform: isInView
+            ? "translateZ(0) translateY(0)"
+            : "translateZ(-700px) translateY(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.4s",
+        }}
+      >
+        <div className="redButton" />
+        <div className="yellowButton" />
+        <div className="greenButton" />
+        <div className="url">DarrenChan.ca</div>
+        <div className="skillsCategory">
+          <div className="purpleText">Skills</div>
+          <div className="firstBracket">{`{`}</div>
+          <div className="category">
+            Languages =
+            <span className="categoryText">
+              HTML + CSS + JavaScript + C# + SQL;
+            </span>{" "}
+          </div>
+          <div className="category">
+            Frameworks =
+            <span className="categoryText">
+              React.JS + .Net Framework + Bootstrap + Node.js;
+            </span>{" "}
+          </div>
+          <div className="category">
+            Databases =
+            <span className="categoryText">MySql + SQLite + MongoDB;</span>{" "}
+          </div>
+          <div className="category">
+            Others =<span className="categoryText">Git;</span>{" "}
+          </div>
+          <p className="endBracket">{`}`}</p>
+        </div>
+        <div className="educationCategory">
+          <div className="purpleText">Education</div>
+          <div className="firstBracket">{`{`}</div>
+          <div className="category">
+            Degree =<span className="categoryText">Bachelor of Computing;</span>
+          </div>
+          <div className="category">
+            Major =<span className="categoryText">Computer Science;</span>
+          </div>
+          <div className="category">
+            Minor =<span className="categoryText">Business Economics;</span>
+          </div>
+          <div className="endBracket">{`}`}</div>
+        </div>
+      </motion.div>
+    </div>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,40 @@
-import { useState } from 'react';
-import './Header.css';
-import Container from "react-bootstrap/Container";
-import dcLogo from "./assets/logo.png";
+import "./Header.css";
+import dcLogo from "./assets/logo.svg";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Image from 'react-bootstrap/Image';
-
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 function HeaderComponent() {
-    return (
-        <Navbar collapseOnSelect expand="lg">
-            <div className="headerContainer d-flex justify-content-start">
-                <Navbar.Brand href="#">
-                    <Image className="personalLogo" src={dcLogo}></Image>
-                </Navbar.Brand>
-            <Nav defaultActiveKey="/home">
-                <Nav.Link href="#Home">Home</Nav.Link>
-                <Nav.Link eventKey={2} href="#AboutMe">
-                    About Me
-                </Nav.Link>
-                <Nav.Link eventKey={3} href="#Projects">
-                    Projects
-                </Nav.Link>
-                <Nav.Link eventKey={4} href="#Contact">
-                    Contact
-                </Nav.Link>
-                </Nav>
-            </div>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="sm">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <Image className="personalLogo" src={dcLogo}></Image>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav defaultActiveKey="/home">
+            <Nav.Link href="#Home">
+              <span className="link-text">Home</span>
+              <span className="linkAnimation"></span>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#AboutMe">
+              <span className="link-text">About Me</span>
+              <span className="linkAnimation"></span>
+            </Nav.Link>
+            <Nav.Link eventKey={3} href="#Projects">
+              <span className="link-text">Projects</span>
+              <span className="linkAnimation"></span>
+            </Nav.Link>
+            <Nav.Link eventKey={4} href="#Contact">
+              <span className="link-text">Contact</span>
+              <span className="linkAnimation"></span>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default HeaderComponent;
